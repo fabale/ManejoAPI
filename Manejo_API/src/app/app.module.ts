c425be4da94ee9daab203884f305ee4c7d5272c3
fabale <alean.sg@gmail.com>
2019-09-15 15:40:48 -0500

    paises

--- Manejo_API/src/app/app.module.ts
+++ Manejo_API/src/app/app.module.ts
@@ -9,10 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  // tslint:disable-next-line: deprecation
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
--- Manejo_API/src/app/tab2/tab2.page.ts
+++ Manejo_API/src/app/tab2/tab2.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CountryService} from '../api/country.service';
+import { error } from 'util';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  paises: any[] = [];
+  errorMessage = '';
+  filtro: any[] = [];
+  filteredProducts: any[] = [];
 
-  constructor() {}
+  constructor(private countryService: CountryService) {}
 
+  ngOnInit() {
+this.countryService.obtenerPaises().subscribe(
+
+  paises => {
+this.paises = paises;
+  }, err  => this.errorMessage = error as any
+);
+console.log(this.paises);
+  }
 }
